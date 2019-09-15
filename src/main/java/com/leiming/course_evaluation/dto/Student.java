1abb33288a35d8a3fa708a348d070bd7de7a12e3
Ming Lei <1079626899@qq.com>
2019-09-15 18:01:02 +0800

    增加了一些页面以及完善了部分数据库的实体对应关系

--- src/main/java/com/leiming/course_evaluation/dto/Student.java
+++ src/main/java/com/leiming/course_evaluation/dto/Student.java
@@ -1,20 +1,39 @@
 package com.leiming.course_evaluation.dto;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
 //学生实体
 @Entity
+
+
 public class Student {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String password;
-    private String stuNumber;
     private String sex;
+    private String stuNumber;
+    @Transient
     private String className;
+    @Transient
+    private String dptName;
+    @ManyToOne
+    @JoinColumn(name = "cg_class_id")
+    @JsonBackReference
+    private CgClass cgClass;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    @JsonBackReference
+    private Department department;
+
+
+
     protected Student(){
 
     }
@@ -43,20 +62,20 @@ public class Student {
         this.password = password;
     }
 
-    public String getStuNumber() {
-        return stuNumber;
-    }
-
-    public void setStuNumber(String stuNumber) {
-        this.stuNumber = stuNumber;
-    }
-
     public String getSex() {
         return sex;
     }
 
     public void setSex(String sex) {
         this.sex = sex;
+    }
+
+    public String getStuNumber() {
+        return stuNumber;
+    }
+
+    public void setStuNumber(String stuNumber) {
+        this.stuNumber = stuNumber;
     }
 
     public String getClassName() {
@@ -67,11 +86,38 @@ public class Student {
         this.className = className;
     }
 
-    public Student(String username, String password, String stuNumber, String sex, String className) {
+    public String getDptName() {
+        return dptName;
+    }
+
+    public void setDptName(String dptName) {
+        this.dptName = dptName;
+    }
+
+    public CgClass getCgClass() {
+        return cgClass;
+    }
+
+    public void setCgClass(CgClass cgClass) {
+        this.cgClass = cgClass;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public Student(String username, String password, String sex, String stuNumber, String className, String dptName, CgClass cgClass, Department department) {
         this.username = username;
         this.password = password;
-        this.stuNumber = stuNumber;
         this.sex = sex;
+        this.stuNumber = stuNumber;
         this.className = className;
+        this.dptName = dptName;
+        this.cgClass = cgClass;
+        this.department = department;
     }
 }
