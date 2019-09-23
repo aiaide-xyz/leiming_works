343a02f774cf611c6574a7529ec8b506a9abf36b
Ming Lei <1079626899@qq.com>
2019-09-23 10:22:14 +0800

    完成修改学生信息功能

--- src/main/java/com/leiming/course_evaluation/controller/comment/Logout.java
+++ src/main/java/com/leiming/course_evaluation/controller/comment/Logout.java
@@ -1,20 +1,20 @@
 package com.leiming.course_evaluation.controller.comment;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
-@RestController
+@Controller
 @RequestMapping("/logout")
 //统一退出登录
 public class Logout {
     @GetMapping("")
     public String logout(HttpServletRequest request){
         if (request.getSession().getAttribute("user")==null){
-            return "请先登录";
+            return "redirect:login";
         }
         request.getSession().removeAttribute("user");
-        return "退出成功";
+        return "redirect:login";
     }
 }
